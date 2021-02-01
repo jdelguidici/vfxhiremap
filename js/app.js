8f1c6586608cd548b71384c410ff7da208ed8786
David Thorpe <djt@mutablelogic.com>
2021-02-01 15:26:23 +0100

    Updated spider params

--- js/app.js
+++ js/app.js
@@ -91,9 +91,15 @@ class App {
         this.details = new bootstrap.Collapse(document.getElementById('details'), {
             toggle: false,
         });
+        document.getElementById('details').addEventListener('show.bs.collapse', () => {
+            // Set maximum height to the height of the map 
+            document.getElementById('details').style.maxHeight = document.getElementById('map').clientHeight + "px";
+        });
         document.getElementById('details').addEventListener('shown.bs.collapse', () => {
             // Reset map state
             this.map.invalidateSize();
+
+            console.log("shown=",document.getElementById('details').clientHeight);
         });
         document.getElementById('details').addEventListener('hidden.bs.collapse', () => {
             // Reset map state
@@ -144,6 +150,7 @@ class App {
         this.loadButtons();
         this.markers = L.markerClusterGroup({
             maxClusterRadius: 25,
+            spiderfyDistanceMultiplier: 2,
             iconCreateFunction: this.markerView.Cluster.bind(this.markerView),
         });
         this.map.addLayer(this.markers);
@@ -191,8 +198,6 @@ class App {
         this.detailView.Set(row);
         // Show details
         this.details.show();
-        // Update map size
-        this.map.invalidateSize();
         // Center the marker in the map
         this.map.panTo(marker.getLatLng());
     }
